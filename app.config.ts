5b362ad05d19b1d29ed6a2d31cd6f1ce8fc99519
Darren Candra <darrencndr@gmail.com>
2024-03-21 00:30:03 +1100

    remove redundant app config

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,3 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({
-  // @ts-ignore
-  start: {
-    server: {
-      preset: "cloudflare-pages",
-      rollupConfig: {
-        external: ["node:async_hooks"]
-      },
-    }
-  }
-});
+export default defineConfig({});
