95dbbbff5c8f029ca820c3b3c6de35d6a9eb4a8f
Darren Candra <darrencndr@gmail.com>
2024-03-20 23:57:23 +1100

    nvm

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,3 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({
-  // @ts-ignore
-  start: {
-    server: {
-      preset: "cloudflare-pages"
-    }
-  }
-});
+export default defineConfig({});
